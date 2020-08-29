022dbd15564390b6fd17d159f1a627216daf843c
sunnyshubham <37530501+sunnyshubham@users.noreply.github.com>
2020-08-29 18:33:57 +0530

    done

--- assign.py
+++
@@ -1,6 +0,0 @@
-data={"username":"myname","password":"me","details":{"email":"a@gmail.com","phone":9989}}
-username=data["username"]
-password=data["password"]
-print(username,password)
-print(data["details"]["email"])
-print(data["details"]["phone"])
